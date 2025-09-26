b1ef1025003cde363efafc7bce93d5023f0d381e
Rivers Chen <river.chen@uwaterloo.ca>
2025-09-26 09:44:37 -0400

    Turns out sanoid is already in ucore

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,15 +5,3 @@ systemctl enable nfs-server.service
 systemctl enable smb.service
 systemctl enable cockpit.socket
 
-
-#install sanoid
-cd /tmp
-sudo git clone https://github.com/jimsalterjrs/sanoid.git
-cd sanoid
-git checkout $(git tag | grep "^v" | tail -n 1)
-sudo cp sanoid syncoid findoid sleepymutex /usr/local/sbin
-sudo mkdir /etc/sanoid
-sudo cp sanoid.defaults.conf /etc/sanoid
-sudo touch /etc/sanoid/sanoid.conf
-sudo cp sanoid.conf /etc/sanoid/sanoid.example.conf
-
