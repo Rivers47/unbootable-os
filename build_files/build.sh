83cacec3fd04663ea1ef199dd4f820dd69ae96f3
Rivers Chen <rivers.chen@uwaterloo.ca>
2025-10-30 21:15:30 -0400

    Use curl because there is no wget

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dnf install -y vim fzf butane cockpit htop zoxide bfs
 
-wget -O /tmp/7z.tar.xz https://www.7-zip.org/a/7z2501-linux-x64.tar.xz
+curl --output /tmp/7z.tar.xz https://www.7-zip.org/a/7z2501-linux-x64.tar.xz
 tar xvf /tmp/7z.tar.xz 7zz 7zzs
 mv 7zz 7zzs /usr/bin
 
